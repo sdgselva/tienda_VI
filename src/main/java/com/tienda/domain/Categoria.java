020f221a1ac226ae4c3584efdba284dbb8a9fce4
Sol Gutierrez <sdgselva@gmail.com>
2025-02-07 13:46:39 -0600

    Semana 4 SQl connection and remote DB creation and connection

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,53 @@
+
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable{
+   private static final long serialVersionUID = 1L;
+    
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   
+   private Long idCategoria;
+   private String descripcion;
+   private String rutaImagen;
+   private boolean activo;
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripicion) {
+        this.descripcion = descripicion;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import com.tienda.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activos){
+        var lista = categoriaRepository.findAll();
+        return lista;
+    }
+}
